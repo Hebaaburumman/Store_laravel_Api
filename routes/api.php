a2fa91188cad8c509113da263d80e6a91a50b1ea
Hebaaburumman <144600608+Hebaaburumman@users.noreply.github.com>
2024-02-25 12:22:02 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -50,15 +50,9 @@ Route::get('/users', [UserController::class, 'list']);//done
     Route::delete('/products/{id}', [ProductController::class, 'delete']); //done
     Route::put('/products/{id}/remove-quantity', [ProductController::class, 'removeOneQuantity']);//done
 
-// routes/api.php
-
-
-// Route::middleware('auth:api')->group(function () {
-//     Route::post('/products', [ProductController::class, 'store']);
-// });
 
 // category // all done
-    Route::get('/categories', [CategoryController::class, 'index']); //done the products appear with category
+    Route::get('/categories', [CategoryController::class, 'index']); //done 
     Route::post('/categories', [CategoryController::class, 'store']); //done
     Route::get('/categories/{id}/edit', [CategoryController::class, 'edit']); //done
     Route::put('/categories/{id}', [CategoryController::class, 'update']); // done
@@ -67,14 +61,10 @@ Route::get('/users', [UserController::class, 'list']);//done
     
 //dashboard
     Route::get('/dashboard', [DashboardController::class, 'dashboard']); //done
-    // Route::get('/dashboard/products-by-category', [DashboardController::class, 'productsByCategory']);
 
 
 
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 
 
